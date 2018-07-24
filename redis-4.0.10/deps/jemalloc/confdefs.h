a0d575de614ec5e4a231c76fde2b7f6fb4c5edb4
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-24 19:04:25 +0000

    automatically backed up by learn

--- redis-4.0.10/deps/jemalloc/confdefs.h
+++ redis-4.0.10/deps/jemalloc/confdefs.h
@@ -5,3 +5,32 @@
 #define PACKAGE_STRING ""
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_URL ""
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define SIZEOF_VOID_P 8
+#define LG_SIZEOF_PTR 3
+#define SIZEOF_INT 4
+#define LG_SIZEOF_INT 2
+#define SIZEOF_LONG 8
+#define LG_SIZEOF_LONG 3
+#define SIZEOF_INTMAX_T 8
+#define LG_SIZEOF_INTMAX_T 3
+#define CPU_SPINWAIT __asm__ volatile("pause")
+#define JEMALLOC_HAS_ALLOCA_H 1
+#define JEMALLOC_PURGE_MADVISE_DONTNEED  
+#define JEMALLOC_THREADED_INIT  
+#define JEMALLOC_USE_CXX_THROW  
+#define HAVE_MALLOC_H 1
+#define JEMALLOC_USABLE_SIZE_CONST 
+#define JEMALLOC_HAVE_ATTR  
+#define JEMALLOC_TLS_MODEL __attribute__((tls_model("initial-exec")))
+#define JEMALLOC_HAVE_ATTR_ALLOC_SIZE  
+#define JEMALLOC_HAVE_ATTR_FORMAT_GNU_PRINTF  
--- redis-4.0.10/deps/jemalloc/conftest.c
+++ redis-4.0.10/deps/jemalloc/conftest.c
@@ -5,53 +5,41 @@
 #define PACKAGE_STRING ""
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_URL ""
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define SIZEOF_VOID_P 8
+#define LG_SIZEOF_PTR 3
+#define SIZEOF_INT 4
+#define LG_SIZEOF_INT 2
+#define SIZEOF_LONG 8
+#define LG_SIZEOF_LONG 3
+#define SIZEOF_INTMAX_T 8
+#define LG_SIZEOF_INTMAX_T 3
+#define CPU_SPINWAIT __asm__ volatile("pause")
+#define JEMALLOC_HAS_ALLOCA_H 1
+#define JEMALLOC_PURGE_MADVISE_DONTNEED  
+#define JEMALLOC_THREADED_INIT  
+#define JEMALLOC_USE_CXX_THROW  
+#define HAVE_MALLOC_H 1
+#define JEMALLOC_USABLE_SIZE_CONST 
+#define JEMALLOC_HAVE_ATTR  
+#define JEMALLOC_TLS_MODEL __attribute__((tls_model("initial-exec")))
+#define JEMALLOC_HAVE_ATTR_ALLOC_SIZE  
+#define JEMALLOC_HAVE_ATTR_FORMAT_GNU_PRINTF  
 /* end confdefs.h.  */
-#include <stdarg.h>
-#include <stdio.h>
-struct stat;
-/* Most of the following tests are stolen from RCS 5.7's src/conf.sh.  */
-struct buf { int x; };
-FILE * (*rcsopen) (struct buf *, struct stat *, int);
-static char *e (p, i)
-     char **p;
-     int i;
-{
-  return p[i];
-}
-static char *f (char * (*g) (char **, int), char **p, ...)
-{
-  char *s;
-  va_list v;
-  va_start (v,p);
-  s = g (p, va_arg (v,int));
-  va_end (v);
-  return s;
-}
-
-/* OSF 4.0 Compaq cc is some sort of almost-ANSI by default.  It has
-   function prototypes and stuff, but not '\xHH' hex character constants.
-   These don't provoke an error unfortunately, instead are silently treated
-   as 'x'.  The following induces an error, until -std is added to get
-   proper ANSI mode.  Curiously '\x00'!='x' always comes out true, for an
-   array size at least.  It's necessary to write '\x00'==0 to get something
-   that's true only with -std.  */
-int osf4_cc_array ['\x00' == 0 ? 1 : -1];
-
-/* IBM C 6 for AIX is almost-ANSI by default, but it replaces macro parameters
-   inside strings and character constants.  */
-#define FOO(x) 'x'
-int xlc6_cc_array[FOO(a) == 'x' ? 1 : -1];
-
-int test (int i, double x);
-struct s1 {int (*f) (int a);};
-struct s2 {int (*f) (double a);};
-int pairnames (int, char **, FILE *(*)(struct buf *, struct stat *, int), int, int);
-int argc;
-char **argv;
+#include <stdlib.h>
 int
 main ()
 {
-return f (e, argv, 0) != argv[0]  ||  f (e, argv, 1) != argv[1];
+void *foo(const char *format, ...) __attribute__((format(printf, 1, 2)));
   ;
   return 0;
 }
